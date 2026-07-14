f94369496104065e957e55200810f3c9426675ab
yagodr <y.rodr1gues0811@outlook.com>
2026-07-13 22:39:07 -0300

    feat(api): add OpenAPI documentation

--- document-api/src/main/java/com/yago/asyncdocx/api/document/DocumentDataController.java
+++ document-api/src/main/java/com/yago/asyncdocx/api/document/DocumentDataController.java
@@ -1,5 +1,9 @@
 package com.yago.asyncdocx.api.document;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/document-data")
+@Tag(name = "Document Data", description = "Stores service agreement input data.")
 public class DocumentDataController {
 
 	private final DocumentDataService service;
@@ -20,6 +25,13 @@ public class DocumentDataController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@Operation(
+			summary = "Create document data",
+			description = "Stores the service agreement data that will be used later by an asynchronous document generation request.",
+			responses = {
+					@ApiResponse(responseCode = "201", description = "Document data was created."),
+					@ApiResponse(responseCode = "400", description = "The request body is invalid.", content = @Content)
+			})
 	CreateDocumentDataResponse create(@Valid @RequestBody CreateDocumentDataRequest request) {
 		DocumentData documentData = service.create(request);
 		return new CreateDocumentDataResponse(documentData.getId());
